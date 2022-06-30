aa847aa54babc4e2c8ad4244fe42a5b160416aee
omaks45 <danielgozie5@gmail.com>
2022-06-30 10:57:57 -0700

    nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - main block
+ *
  * Return: 0
  */
-
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
@@ -13,4 +12,4 @@ int main(void)
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
-}	
+}
