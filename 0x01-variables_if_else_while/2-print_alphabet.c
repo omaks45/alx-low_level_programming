88a91bc77e02c5845d95ca9b0e1eca6b13b88c32
omaks45 <danielgozie5@gmail.com>
2022-07-03 07:28:43 -0700

    letter conversion

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main Block
+ * Description: convert all the alphabet to small letters
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a'
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
