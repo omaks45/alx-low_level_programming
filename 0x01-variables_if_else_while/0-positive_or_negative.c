42aa23a7fc385fdc1066a2dc1962fa28c4e4390d
omaks45 <danielgozie5@gmail.com>
2022-07-03 04:57:53 -0700

    determination of number change

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,14 @@
 /**
  * main - Entry point
  *
- * Reurn: Always 0 (Success)
+ * Description: Get a random number and check if it is a positive, negative or zero
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+
+	int;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -16,7 +20,7 @@ int main(void)
 	printf("%d is positive\n", n);
 	}
 	if (n < 0)
-	{ 
+	{
 	printf("%d is negative\n", n);
 	}
 	if (n == 0)
