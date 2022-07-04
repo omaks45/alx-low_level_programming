f23a6988c46f28cf1746854d50bfcb641787672d
omaks45 <danielgozie5@gmail.com>
2022-07-04 08:00:46 -0700

    number printing

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-	int c;
+	int c = '0';
 
-	for (c = 0; c <= 9; c++)
+	while (c <= '9')
 	{
-		printf("%d\n", c);
+		putchar(c);
+		c++;
 	}
+	putchar('\n');
 	return (0);
 }
