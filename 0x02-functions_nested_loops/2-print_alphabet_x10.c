da6632cee574e7cb29a0d15bbb24cc885c2410e6
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-07-06 17:03:41 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
-* print_alphabet_x10 - Print the alphabet ten times in lowercase and a new line
+ *print_alphabet_x10 - print alphabet 10 times, in lowercase on a new line.
 */
+
 void print_alphabet_x10(void)
 {
 	int times = 0;
 	char lett;
 
-        while (times++ <= 9)
+	while (times++ <= 9)
 	{
 		for (lett = 97; lett <= 122; lett++)
-		      _putchar(lett);	
+			_putchar(lett);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
