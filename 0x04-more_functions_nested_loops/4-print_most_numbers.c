0dc1473f3ddf13e647a49e525bb7b554216afba9
omaks45 <danielgozie5@gmail.com>
2022-07-07 10:40:19 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_most_numbers - print numbers
+*/
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (j != 2 && j != 4)
+			putchar(j + '0');
+	}
+	putchar('\n');
+}
