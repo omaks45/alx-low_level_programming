ff0ca6da1a8fcf5213c3e1a2f3d01cdc0aa8428f
omaks45 <danielgozie5@gmail.com>
2022-07-07 15:25:25 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,9 +6,9 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 15; b++)
+		for (b = 0; b < 15; b++)
 		{
 			if (b >= 10)
 				_putchar((b / 10) + '0');
