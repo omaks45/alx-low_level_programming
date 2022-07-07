733847ebdc80af5b6cec057a983c64474d5903cd
omaks45 <danielgozie5@gmail.com>
2022-07-07 11:13:54 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,8 +7,9 @@ void more_numbers(void)
 	int a, b;
 
 	for (a = 0; a <= 10; a++)
-		_putchar(a + '0');
-	for (b = 0; b <= 15; b++)
-		_putchar(b + '0');
-	putchar('\n');
+	{
+		for (b = 0; b <= 15; b++)
+			_putchar(b + '0');
+	}
+	_putchar('\n');
 }
