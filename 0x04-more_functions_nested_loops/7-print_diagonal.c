f8cd82f1bf7666b9441a56015070f3d81d4e53bc
omaks45 <danielgozie5@gmail.com>
2022-07-08 06:43:31 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
 * print_diagonal - print a diagonal line
+* @n: integer value
 */
 void print_diagonal(int n)
 {
-	int x, j;
+	int v, u;
 
-	for (n <= 0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (x = 0; x < n; x++)
-		{
-			for (j = 0; j < x; j++)
+		for (v = 0; v < n; v++)
+			for (u = 0; u < v u++)
 				_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
-		}
+		_putchar(92);
+		_putchar('\n');
 	}
 }
