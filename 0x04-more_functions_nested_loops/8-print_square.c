067743745922bb9375b4ac395cd13c26d3d633c4
omaks45 <danielgozie5@gmail.com>
2022-07-08 07:53:35 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_square - print a square
+* @size: integer value
+*/
+void print_square(int size)
+{
+	int t, h;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (t = 0; t < size; t++)
+		{
+			for (h = 0; h < size; h++)
+				_putchar(35);
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
