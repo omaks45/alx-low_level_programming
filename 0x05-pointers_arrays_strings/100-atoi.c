2da8029f06ae245c840cc5714e296053434d4963
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-07-13 09:17:06 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _atoi - converts a string to an integer
-* @s: string to be converted
-* Return: integers in a string
-*/
+ * _atoi - converts a string to an integer
+ *@s: string to be converted
+ * Return: integers in a string
+ */
 
 int _atoi(char *s)
 {
