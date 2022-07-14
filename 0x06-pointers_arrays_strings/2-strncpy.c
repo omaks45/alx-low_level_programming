f36b6e51e837185071f3d579b684fb0c7f31e022
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-07-14 15:08:25 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,55 +1,28 @@
 #include "main.h"
 
-
-
 /**
+ *_strncpy - function that copies a string
  *
- *  *_strncpy - function that copies a string
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
  *
- *   *
- *
- *    * @src: The source of strings
- *
- *     * @dest: The destination of the string
- *
- *      * @n: The length of int
- *
- *       *
- *
- *        * Return: pointer to the resulting string dest
- *
- *         */
-
+ * Return: pointer to the resulting string dest
+ */
 char *_strncpy(char *dest, char *src, int n)
-
 {
+int i;
 
-	int i;
-
-
-
-		for (i = 0; i < n && *(src + i); i++)
-
-				{
-
-							*(dest + i) = *(src + i);
-
-								}
-
-			for (; i < n; i++)
-
-					{
-
-							*(dest + i) = '\0';
-
-								}
-
-				return (dest);
-
-
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
+	return (dest);
 
 }
-
 Footer
-
 © 2022 GitHub, Inc.
