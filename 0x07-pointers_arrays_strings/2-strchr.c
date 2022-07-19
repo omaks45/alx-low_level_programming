f8e544f5f0ad998359f733609dc97194c01760d4
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-07-19 01:46:08 +0100

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,51 +1,27 @@
 #include "main.h"
-
-
+#include <string.h>
 
 /**
+ * _strchr - locates a character in a string
  *
- *  * _strchr - locates a character in a string
+ * @c: occurrence of the character
  *
- *   *
+ * @s: in the string
  *
- *    * @c: occurrence of the character
- *
- *     *
- *
- *      * @s: in the string
- *
- *       *
- *
- *        * Return: a pointer to the first occurrence of the character
- *
- *         */
-
-
+ * Return: a pointer to the first occurrence of the character
+ */
 
 char *_strchr(char *s, char c)
-
 {
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
 
-		while (*s)
-
-				{
-
-							if (*s != c)
-
-											s++;
-
-									else
-
-													return (s);
-
-										}
-
-			if (c == '\0')
-
-						return (s);
-
-
-
-				return (NULL);
-
+	return (NULL);
 }
