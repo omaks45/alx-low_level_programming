cb5a683f4ef823b5040acfdfcb40d9693d3e1f30
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-07-21 20:23:10 +0100

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,41 +1,21 @@
 #include "main.h"
-
 /**
+ * _strlen_recursion -  function that returns the length of a string
  *
- *  * _strlen_recursion -  function that returns the length of a string
+ * @s: the pointer of char
  *
- *   *
- *
- *    * @s: the pointer of char
- *
- *     *
- *
- *      * Return: the value length
- *
- *       */
-
-
+ * Return: the value length
+ */
 
 int _strlen_recursion(char *s)
 
-
-
 {
+	int length = 0;
 
-		int length = 0;
-
-
-
-			if (*s)
-
-					{
-
-								length++;
-
-										length += _strlen_recursion(s + 1);
-
-											}
-
-				return (length);
-
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
 }
