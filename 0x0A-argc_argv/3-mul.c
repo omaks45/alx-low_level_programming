711210545ae51d0092c329e7cc22934eb79c4308
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-07-25 15:28:50 +0100

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,49 +1,24 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdio.h>
-
-#include <stdlib.h>
-
 /**
+ * main - multiplies two numbers
+ * @argc: argument counter
+ * @argv: array of pointer to strings
  *
- *  * main - multiplies two numbers
- *
- *   * @argc: argument counter
- *
- *    * @argv: array of pointer to strings
- *
- *     *
- *
- *      * Return: 0
- *
- *       */
-
+ * Return: 0
+ */
 int main(int argc, char *argv[])
-
 {
+	int i, j, k;
 
-		int i, j, k;
-
-
-
-			i = 1;
-
-				j = 2;
-
-					k = 3;
-
-						if (argc != k)
-
-								{
-
-											printf("Error\n");
-
-													return (1);
-
-														}
-
-							printf("%d\n", atoi(argv[i]) * atoi(argv[j]));
-
-								return (0);
-
+	i = 1;
+	j = 2;
+	k = 3;
+	if (argc != k)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[i]) * atoi(argv[j]));
+	return (0);
 }
