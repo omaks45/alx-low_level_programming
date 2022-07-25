aa02f4a4e11e0c22fb619f3c9446a3ba2d2823ea
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-07-25 15:20:23 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,33 +1,17 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
+ * main - the entry point
+ * @argc: argument counter
+ * @argv: array of pointer to strings
  *
- *  * main - the entry point
- *
- *   * @argc: argument counter
- *
- *    * @argv: array of pointer to strings
- *
- *     *
- *
- *      * Return: 0
- *
- *       */
-
-
-
+ * Return: 0
+ */
 
 
 int main(int argc, char *argv[] __attribute__ ((unused)))
 
-
-
 {
-
-		printf("%d\n", argc - 1);
-
-			return (0);
-
+	printf("%d\n", argc - 1);
+	return (0);
 }
