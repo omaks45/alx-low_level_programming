ffdb6befddf14079367b19d261aa28f970cf3703
omaks45 <danielgozie5@gmail.com>
2022-07-29 03:11:24 -0700

    0x0xC-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* malloc_checked - check for malloc
+* @b: parameter b
+* Return: ptr to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+Task 0:Trust no one
+a function that allocates memory using malloc and when malloc failed it had its normal process termination with a status value of 98.
