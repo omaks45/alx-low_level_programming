71e8be24c43fba8b90e49963fe3273b16587e8f6
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-08-02 13:43:31 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,63 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
-* _strlen - returns the length of a string
-* @s: string to be evaluated
-*
-* Return: the length of the string
-*/
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-	int = 0;
+	int i;
+
+	i = 0;
+
 	while (s[i] != '\0')
 	{
-		i++
+		i++;
 	}
+
 	return (i);
 }
 
 /**
-* *_strcpy - copies the string pointed to by orgn
-* with terminating null byte
-* @ptr: pointer to the buffer which we copy string
-* @orgn: string to be copied
-*
-* Return the pointer to ptr
-*/
-char *_strcpy(char *ptr, char *orgn)
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
-	while (orgn[len] != '\0')
+	len = 0;
+
+	while (src[len] != '\0')
 	{
 		len++;
 	}
+
 	for (i = 0; i < len; i++)
 	{
-		ptr[i] = orgn[i];
+		dest[i] = src[i];
 	}
-	ptr[i] = '\0';
-	return (ptr);
+	dest[i] = '\0';
+
+	return (dest);
 }
 
 /**
-* new_dog - creates a new dog
-* @name: name of the dog
-* @age: age of the dog
-* @owner: owner of the dog
-* Return: pointer to the new dog (Success), NULL else
-*
-*/
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
@@ -59,6 +69,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
@@ -69,7 +80,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog->owner == NULL)
 	{
 		free(dog);
-		free(dog->owner);
+		free(dog->name);
 		return (NULL);
 	}
 	_strcpy(dog->name, name);
