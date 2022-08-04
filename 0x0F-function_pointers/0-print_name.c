7e1c354aa4c2e22830d2d51f1aa6e2a4e25946b0
omaks45 <danielgozie5@gmail.com>
2022-08-04 05:25:36 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,18 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	 print_name("Bob", print_name_as_is);
+	 print_name("Bob Dylan", print_name_uppercase);
+	 printf("\n");
+		return (0);
+}
+/**
 * print_name - a function that prints a name
 * @name: name asked to be printed
 * @f: function that points to a name
@@ -9,7 +21,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL && name == NULL)
-		return ;
-		f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
