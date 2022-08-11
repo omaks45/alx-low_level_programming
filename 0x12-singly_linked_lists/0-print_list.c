610ccc4c3709556aa85ae2a7318384485305820a
omaks45 <danielgozie5@gmail.com>
2022-08-11 11:42:57 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+* print_list - prints all the elements of list
+* @h: singly linked list
+* Return: number of elements in the list.
+*/
+size_t print_list(const list_t *h)
+{
+	total = 0;
+
+	while (h != NULL)
+	{
+		if (h != NULL)
+			printf("%d %s\n, 0, (nil)");
+		else
+			printf("%d %s\n", h->len, h->str);
+		h = h->next;
+		total++;
+	}
+	return (total);
+}
