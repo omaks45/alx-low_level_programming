d744a29618f6a6bb895c486ef5293a26d4b5a2a0
omaks45 <danielgozie5@gmail.com>
2022-08-12 06:05:03 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include "lists.h"
 /**
+* _strlen - function that returns len of strings
+* @x: is a character
+* Return: len of string
+*/
+int _strlen(const char *x)
+{
+	int i = 0, count = 0;
+
+	for (; x[i] != '\0'; i++)
+		count += x[i];
+	return (count);
+}
+
+/**
 * add_node - adds a new node to the beginning of list_s lists.
 * @head: head of the linked list
 * @str: string to store in the lists
@@ -11,16 +25,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	size_t i = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->str = strdup(str);
 
-	for (; str[i]; i++)
-		;
-
-	new_node->len = i;
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
