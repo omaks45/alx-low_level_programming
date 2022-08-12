7bfe38aa37a686a87afc5743a0ca24dcce75bdec
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-08-12 14:32:46 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,23 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 /**
-* print_list - prints all the elements of list
-* @h: singly linked list
-* Return: number of elements in the list.
-*/
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
 size_t print_list(const list_t *h)
 {
-	size_t total;
-	total = 0;
+	size_t x;
 
+	x = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n 0, (nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		total++;
+		x++;
 	}
-	return (total);
+	return (x);
 }
