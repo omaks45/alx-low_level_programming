23ef1325f9131eb861029f38528d6e33256d1521
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-08-16 14:10:30 +0100

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,21 +1,35 @@
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-/**
-* pop_listint - deletes head of a node
-* @head: pointer to linked list
-* Return: the head's nodes data
-*/
-int pop_listint(listint_t **head)
-{
-	listint_t *temp;
-	int n;
 
-	temp = *head;
-	if (temp == NULL)
-		return (0);
-	*head = temp->next;
-	n = temp->n;
-	free(temp);
-	return (n);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    int n;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    n = pop_listint(&head);
+    printf("- %d\n", n);
+    print_listint(head);
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
+    return (0);
 }
