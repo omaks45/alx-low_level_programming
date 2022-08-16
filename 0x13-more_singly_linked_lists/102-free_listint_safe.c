f28556db388e8c5543da233b7a340a509bed3b73
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-08-16 18:24:18 +0100

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,143 +1,72 @@
 #include "lists.h"
 
-
-
 /**
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
  *
- *  * free_listp2 - frees a linked list
- *
- *   * @head: head of a list.
- *
- *    *
- *
- *     * Return: no return.
- *
- *      */
-
+ * Return: no return.
+ */
 void free_listp2(listp_t **head)
-
 {
+	listp_t *temp;
+	listp_t *ptr;
 
-		listp_t *temp;
-
-			listp_t *curr;
-
-
-
-				if (head != NULL)
-
-						{
-
-									curr = *head;
-
-											while ((temp = curr) != NULL)
-
-														{
-
-																		curr = curr->next;
-
-																					free(temp);
-
-																							}
-
-													*head = NULL;
-
-														}
-
+	if (head != NULL)
+	{
+		ptr = *head;
+		while ((temp = ptr) != NULL)
+		{
+			ptr = ptr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
 
-
-
 /**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
  *
- *  * free_listint_safe - frees a linked list.
- *
- *   * @h: head of a list.
- *
- *    *
- *
- *     * Return: size of the list that was freed.
- *
- *      */
-
+ * Return: size of the list that was freed.
+ */
 size_t free_listint_safe(listint_t **h)
-
 {
+	size_t new_nodes = 0;
+	listp_t *ptr, *temp, *add;
+	listint_t *prev;
 
-		size_t nnodes = 0;
+	ptr = NULL;
+	while (*h != NULL)
+	{
+		temp = malloc(sizeof(listp_t));
 
-			listp_t *hptr, *new, *add;
+		if (temp == NULL)
+			exit(98);
 
-				listint_t *curr;
+		temp->p = (void *)*h;
+		temp->next = ptr;
+		ptr = temp;
 
+		add = ptr;
 
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&ptr);
+				return (new_node);
+			}
+		}
 
-					hptr = NULL;
+		prev = *h;
+		*h = (*h)->next;
+		free(prev);
+		new_node++;
+	}
 
-						while (*h != NULL)
-
-								{
-
-											new = malloc(sizeof(listp_t));
-
-
-
-													if (new == NULL)
-
-																	exit(98);
-
-
-
-															new->p = (void *)*h;
-
-																	new->next = hptr;
-
-																			hptr = new;
-
-
-
-																					add = hptr;
-
-
-
-																							while (add->next != NULL)
-
-																										{
-
-																														add = add->next;
-
-																																	if (*h == add->p)
-
-																																					{
-
-																																										*h = NULL;
-
-																																														free_listp2(&hptr);
-
-																																																		return (nnodes);
-
-																																																					}
-
-																																			}
-
-
-
-																									curr = *h;
-
-																											*h = (*h)->next;
-
-																													free(curr);
-
-																															nnodes++;
-
-																																}
-
-
-
-							*h = NULL;
-
-								free_listp2(&hptr);
-
-									return (nnodes);
-
+	*h = NULL;
+	free_listp2(&ptr);
+	return (new_node);
 }
