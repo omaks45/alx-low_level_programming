5464461973358ef7d67e9d9443e6675f04513fe2
omaks45 <danielgozie5@gmail.com>
2022-08-16 05:29:59 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <stddef.h>
 #include "lists.h"
 /**
@@ -12,8 +10,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while ((head = temp) != NULL)
-	head = head->next;
-	free(temp->next);
-	free(temp);
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
