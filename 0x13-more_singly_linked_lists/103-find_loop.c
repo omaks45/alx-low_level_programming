6dbc7319e1ba5ff794c39f704d91146b1ea295fc
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-08-16 18:09:45 +0100

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,77 +1,39 @@
 #include "lists.h"
-
 /**
- *
- *  * find_listint_loop - finds the loop contained
- *
- *   *                     in a linked list
- *
- *    * @head : pointer to the head of the linked list
- *
- *     * Return: pointer the node where the loop starts,
- *
- *      *         or NULL - if there is no loop
- *
- *       */
-
+ * find_listint_loop - finds the loop contained
+ *                     in a linked list
+ * @head : pointer to the head of the linked list
+ * Return: pointer the node where the loop starts,
+ *         or NULL - if there is no loop
+ */
 listint_t *find_listint_loop(listint_t *head)
-
 {
+listint_t *loop1, *loop2;
 
-	listint_t *node1, *node2;
+if (head == NULL || head->next == NULL)
+return (NULL);
 
+loop1 = head->next;
+loop2 = (head->next)->next;
 
+while (loop2)
+{
+if (loop1 == loop2)
+{
+loop1 = head;
 
-	if (head == NULL || head->next == NULL)
+while (loop1 != loop2)
+{
+loop1 = loop1->next;
+loop2 = loop2->next;
+}
 
-		return (NULL);
+return (loop1);
+}
 
+loop1 = loop1->next;
+loop2 = (loop2->next)->next;
+}
 
-
-	node1 = head->next;
-
-	node2 = (head->next)->next;
-
-
-
-	while (node2)
-
-	{
-
-		if (node1 == node2)
-
-		{
-
-			node1 = head;
-
-
-
-			while (node1 != node2)
-
-			{
-
-				node1 = node1->next;
-
-				node2 = node2->next;
-
-			}
-
-
-
-			return (node1);
-
-		}
-
-
-
-		node1 = node1->next;
-
-		node2 = (node2->next)->next;
-
-	}
-
-
-
-	return (NULL);
-
+return (NULL);
 }
