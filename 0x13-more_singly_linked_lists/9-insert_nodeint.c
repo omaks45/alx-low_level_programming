eed65788f6f7d6d977ce63520d30e432822a6ae6
omaks45 <danielgozie5@gmail.com>
2022-08-16 08:11:18 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	for (x = 0; x < (idx - 1) x++)
+	for (x = 0; x < (idx - 1); x++)
 	{
 		if (temp == NULL || temp->next == NULL)
 			return (NULL);
