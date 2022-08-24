f6cab288ec3a765fd7521ad912494903c72693c7
omaks45 <danielgozie5@gmail.com>
2022-08-24 11:16:59 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,13 +14,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_APPEND);
-	if (fd == -1 || count == -1)
+	if (fd == -1)
 		return (-1);
-	if (text_content ==)
+	if (text_content == NULL)
 	{
 		for (i = 0; text_content[i]; i++)
 			;
 		count = write(fd, text_content, i);
+
+		if (count == -1)
+			return (-1);
 	}
 
 	close(fd);
