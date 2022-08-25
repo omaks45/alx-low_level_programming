852375abb0f87b6716056cbf87f07ffc03eb4e53
omaks45 <danielgozie5@gmail.com>
2022-08-25 10:10:18 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,17 +10,17 @@
 */
 void access_io_fptr(int fptr, int fd, char *filename, char permission)
 {
-	if (permission == 'C' && fptr == -1)
+	if (permission == 'c' && fptr == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
 		exit(100);
 	}
-	else if (permission == 'O' && fptr == -1)
+	else if (permission == 'o' && fptr == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", filename);
 		exit(98);
 	}
-	else if (permission == 'W' && fptr == -1)
+	else if (permission == 'w' && fptr == -1)
 		dprintf(STDERR_FILENO, "Error: can't write to %s\n", filename);
 }
 
@@ -44,23 +44,23 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
-	access_io_fptr(fd1, -1, argv[1], 'O');
+	access_io_fptr(fd1, -1, argv[1], 'o');
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permission);
-	access_io_fptr(fd2, -1, argv[2], 'W');
+	access_io_fptr(fd2, -1, argv[2], 'w');
 
 	while (message == 1024)
 	{
 		message = read(fd1, buffer, sizeof(buffer));
 		if (message == -1)
-			access_io_fptr(-1, -1, argv[1], 'O');
+			access_io_fptr(-1, -1, argv[1], 'o');
 		fptr1 = write(fd2, buffer, message);
 		if (fptr1 == -1)
-			access_io_fptr(-1, -1, argv[2], 'W');
+			access_io_fptr(-1, -1, argv[2], 'w');
 	}
 
 	close_fd1 = close(fd1);
-	access_io_fptr(close_fd1, fd1, NULL, 'C');
+	access_io_fptr(close_fd1, fd1, NULL, 'c');
 	close_fd2 = close(fd2);
-	access_io_fptr(close_fd2, fd2, NULL, 'C');
+	access_io_fptr(close_fd2, fd2, NULL, 'c');
 	return (0);
 }
